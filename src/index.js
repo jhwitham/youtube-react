ca22e2edec9f2e4e0904b1e8fb171f0bd1d37b2c
John Herman Witham <john.h.witham@gmail.com>
2018-09-18 19:53:00 +0300

    Create App Dummy component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 // Create a new component -> should produce html
+const App = function() {
+  return <div>Hi!</div>;
+}
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
