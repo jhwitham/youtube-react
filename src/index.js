31aac3c67ede2ae4c341c573c5548bb3acc16ff4
John Herman Witham <john.h.witham@gmail.com>
2018-09-18 20:10:17 +0300

    Restructure App component with ES6

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a new component -> should produce html
-const App = function() {
+const App = () => {
   return <div>Hi!</div>;
 }
 
