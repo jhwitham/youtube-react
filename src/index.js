1e3e0fa71706d2e18f0ad9384ca0c15562be9b20
John Herman Witham <john.h.witham@gmail.com>
2018-09-18 20:07:11 +0300

    Import React and ReactDOM & render component and inject to DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // Create a new component -> should produce html
 const App = function() {
   return <div>Hi!</div>;
@@ -5,3 +8,4 @@ const App = function() {
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
