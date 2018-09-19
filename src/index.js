98c1947e3713d0777e70aa7c6af52dde22ab17e7
John Herman Witham <john.h.witham@gmail.com>
2018-09-19 14:18:52 +0300

    SearchBar return input & import and display on index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 // Domain restricted
 const API_KEY = 'AIzaSyBsiX-Crq1m-qpFTLtnmlp1-J98yT8XP1w';
 
 // Create a new component -> should produce html
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it
