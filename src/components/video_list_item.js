077537dc3ba29155cb081135552aab62aec311d3
John Herman Witham <john.h.witham@gmail.com>
2018-09-19 20:45:50 +0300

    Pull out video props & class styling for display

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
+const VideoListItem = ({video}) => {
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" />
+        </div>
+
+        <div className="media-body">
+          <div className="media-hosting"></div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
