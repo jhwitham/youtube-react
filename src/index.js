1887da347961387a413faf4609009e2702fce58c
John Herman Witham <john.h.witham@gmail.com>
2018-09-19 19:18:11 +0300

    Add constructor and videos empty array state. YT search to Component & ES6 sugar

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import SearchBar from './components/search_bar';
 // Domain restricted
 const API_KEY = 'AIzaSyBsiX-Crq1m-qpFTLtnmlp1-J98yT8XP1w';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
-
 // Create a new component -> should produce html
 class App extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div>
