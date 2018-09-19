588c434cf64bfbd5c209801dbf6fa9347747a57c
John Herman Witham <john.h.witham@gmail.com>
2018-09-19 19:06:54 +0300

    Import Youtube api search & make request to verify it's working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 // Domain restricted
 const API_KEY = 'AIzaSyBsiX-Crq1m-qpFTLtnmlp1-J98yT8XP1w';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 // Create a new component -> should produce html
 const App = () => {
